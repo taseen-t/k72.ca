eebebfb8b19bb17da826eccccf88d610b95ed2d6
Starboy756 <taseentariq5@gmail.com>
2025-09-04 23:50:35 +0500

    made landing page beautiful

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="h-screen w-screen fixed">
        <Video />
     </div>
-    <div className="h-screen w-screen relative flex flex-col">
+    <div className="h-screen w-screen relative pb-3 overflow-hidden flex flex-col justify-between">
         <HomeHeroText/>
         <HomeBottomText/>
     </div>
