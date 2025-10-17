306a47ba836b2d9c076736ec47f74a963411ac43
Starboy756 <taseentariq5@gmail.com>
2025-10-17 22:20:03 +0500

    new added things

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 const HomeBottomText = () => {
   return (
     <div className="font-[font2] flex items-center justify-center gap-4">
-        <Link className="text-[6.5vw] leading-[5.5vw] border-2 border-white uppercase rounded-full px-10 pt-4 hover:border-[#D3FD50] hover:text-[#D3FD50]" to="/projects">Projets</Link>
-        <Link className="text-[6.5vw] leading-[5.5vw] border-2 border-white uppercase rounded-full px-10 pt-4 hover:border-[#D3FD50] hover:text-[#D3FD50]" to="/agence">Agence</Link>
+        <Link className="text-[6.5vw] leading-[5.5vw] border-2 border-white uppercase rounded-full px-14 pt-4 hover:border-[#D3FD50] hover:text-[#D3FD50]" to="/projects">Projets</Link>
+        <Link className="text-[6.5vw] leading-[5.5vw] border-2 border-white uppercase rounded-full px-14 pt-4 hover:border-[#D3FD50] hover:text-[#D3FD50]" to="/agence">Agence</Link>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,87 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useRef } from "react";
 
 
 const Agence = () => {
+
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  
+
+   useGSAP(() => {
+     gsap.to(imageDivRef.current, {
+       scrollTrigger: {
+         trigger: imageDivRef.current,
+         start:"top 30%",
+         end: "top -70%",
+         pin: true,
+          scrub: true,
+       },
+       
+     });
+   });
   return (
     <div>
-        Agence
+      <div className="section1">
+        <div ref={imageDivRef} className="absolute h-[20vw] overflow-hidden w-[15vw] rounded-4xl top-65 left-[30vw]">
+          <img
+          ref={imageRef}
+            className="h-full object-cover w-full "
+            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
+            alt=""
+          />
+        </div>
+        <div className="relative font-[font2]">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7e <br /> Douze
+            </h1>
+          </div>
+          <div className="pl-[40%] mt-20">
+            <p className="text-6xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre
+              curiosité nourrit notre créativité. On reste humbles et on dit non
+              aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+              valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+              faire de bons chiffres à court terme, mais on la tue à long terme.
+              C’est pour ça qu’on s’engage à donner de la perspective, pour
+              bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="section2 h-screen">
+        <div className="absolute h-[20vw] overflow-hidden w-[15vw] rounded-4xl top-65 left-[30vw]">
+          {/* <img
+            className="h-full object-cover w-full "
+            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
+            alt=""
+          /> */}
+        </div>
+        <div className="relative font-[font2]">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7e <br /> Douze
+            </h1>
+          </div>
+          <div className="pl-[40%] mt-20">
+            <p className="text-6xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre
+              curiosité nourrit notre créativité. On reste humbles et on dit non
+              aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+              valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+              faire de bons chiffres à court terme, mais on la tue à long terme.
+              C’est pour ça qu’on s’engage à donner de la perspective, pour
+              bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Agence
